e8680ff00f99f3b1f956f8d78f3c159776790d7c
jack <jackaconroy@gmail.com>
2020-09-09 12:42:47 -0500

    updates mini_capstone

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: "Hat", price: 20, image_path: " ", description: "A black hat with a brim.")
+
+Product.create(name: "Shirt", price: 25, image_path: " ",  description: "A plain, white tee.")
+
+Product.create(name: "Shoes", price: 55, image_path: " ", description: "Some sweet hightops with purple highlights and black suede.")
+
+Product.create(name: "Jeans", price: 60, image_path: " ", description: "Tough denim jeans.")
+
+Product.create(name: "Sweatshirt", price: 50, image_path: " ", description: "A large comfy sweatshirt with a hood and everything.")
